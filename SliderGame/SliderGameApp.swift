812bbe8baa62a284310f5bfddf6ec4d8eb7dbac8
Nickolai Grachev <nickbd95@gmail.com>
2023-12-23 17:45:32 +0300

    Initial Commit

---
+++ SliderGame/SliderGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameApp.swift
+//  SliderGame
+//
+//  Created by Nikolai Grachev on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
